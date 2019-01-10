1e64217ae83199f0a5186c165cbb272fc4d03a1c
Halik <michaelodumosu57@gmail.com>
2019-01-09 20:18:40 -0500

    learned about the various possible error code node can raise, but one must figure out how to deal with them

---
+++ Error/err_invalid_this.js
@@ -0,0 +1,5 @@
+const urlSearchParams = new URLSearchParams('foo=bar&baz=new');
+
+const buf = Buffer.alloc(1);
+urlSearchParams.has.call(buf, 'foo');
+// Throws a TypeError with code 'ERR_INVALID_THIS'
