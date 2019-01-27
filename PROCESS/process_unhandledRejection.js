0bdaafab5daa60721c57d443405a1bfcd54eff04
Halik <michaelodumosu57@gmail.com>
2019-01-27 13:14:33 -0500

    learned useful things about process events especially the myriad made to deal with Promise problems

---
+++ PROCESS/process_unhandledRejection.js
@@ -0,0 +1,7 @@
+const unhandledRejections = new Map();
+process.on('unhandledRejection', (reason, promise) => {
+  unhandledRejections.set(promise, reason);
+});
+process.on('rejectionHandled', (promise) => {
+  unhandledRejections.delete(promise);
+});
